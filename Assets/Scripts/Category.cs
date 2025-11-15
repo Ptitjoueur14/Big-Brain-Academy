093620daadb2bf60700056bdd8b5cad7c6f8636d
Adoniss14 <liam.chevassus@epita.fr>
2025-11-15 13:11:03 +0100

    chore: game setup

---
+++ Assets/Scripts/Category.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public enum GameCategory
+{
+    Identification,
+    Memory,
+    Maths,
+    Analyse, 
+    Perception
+}
+public class Category : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Difficulty.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public enum DifficultyLevel
+{
+    Easy,
+    Medium,
+    Hard,
+    Expert
+}
+public class Difficulty : MonoBehaviour
+{
+    public static DifficultyLevel DifficultyLevel = DifficultyLevel.Easy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
